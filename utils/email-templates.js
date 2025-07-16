5260d9a5e7c76e8d41dbb15d64ba137978ba3517
tusher1612 <fackruddin.tusher@gmail.com>
2025-07-16 14:59:12 +0600

    fix(email): resolve Email template style

--- utils/email-templates.js
+++ utils/email-templates.js
@@ -13,7 +13,7 @@ export const generateEmailTemplate = ({
     <table cellpadding="0" cellspacing="0" border="0" width="100%" style="background-color: #ffffff; border-radius: 10px; overflow: hidden; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);">
         <tr>
             <td style="background-color: #4a90e2; text-align: center;">
-                <p style="font-size: 54px; line-height: 54px; font-weight: 800;">SubDub</p>
+                <p style="font-size: 54px; line-height: 54px; font-weight: 800; color:white;">SubDub</p>
             </td>
         </tr>
         <tr>
@@ -53,7 +53,7 @@ export const generateEmailTemplate = ({
         <tr>
             <td style="background-color: #f0f7ff; padding: 20px; text-align: center; font-size: 14px;">
                 <p style="margin: 0 0 10px;">
-                    SubDub Inc. | 123 Main St, Anytown, AN 12345
+                    SubDub Inc. | 123 Main St, USA, AN 12345
                 </p>
                 <p style="margin: 0;">
                     <a href="#" style="color: #4a90e2; text-decoration: none; margin: 0 10px;">Unsubscribe</a> | 
--- utils/send-email.js
+++ utils/send-email.js
@@ -5,15 +5,11 @@ import { gmail_account, transporter } from "../config/nodemailer.js";
 
 export const sendReminderEmail= async(to,type,subscription)=>{
     if(!to && ! type) throw new Error("Missing required parameters");
-     console.log("type",type);
     const template = emailTemplates.find(template => template.label.trim() === type.trim());
-
-console.log("Type:", type);
-console.log("Label:", template ? template.label : "No template found");
     if(!template) throw new Error("Invalid email");
 
     const mailInfo={
- userName:subscription.userName,
+ userName:subscription.user.name,
   subscriptionName:subscription.name,
   renewalDate:dayjs(subscription.renewalDate).format('MMM D : YYYY'),
    planName: subscription.frequency,
@@ -37,7 +33,7 @@ transporter.sendMail(mailOptions,(error,info)=>{
 return console.log(error,"Error sending email")
   }
   else {
-     return console.log(info.response,"Email sent successfully!")
+     return console.log(info.response, ` Email sent to ${mailOptions.to} successfully`)
   }
  
 });
